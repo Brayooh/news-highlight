51665c6727c6abfb5d1cb5b305421b78a512daee
brayooh <brianndichu.bn@gmail.com>
2019-10-14 14:47:50 +0300

    ad configurations

---
+++ config.py
@@ -0,0 +1,37 @@
+import os
+
+class Config:
+    '''
+    General confirguration parent class
+    '''
+
+    NEWS_API_BASE_URL = "https://newsapi.org/v2/{}?country=us&apiKey={}"
+
+    SOURCES_API_URL = "https:/VI /newsapi.org/v2/sources?apiKey{}"
+
+    NEWS_API_KEY = 'e9848d19677449d9bb53116781820443'
+
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    Args:
+        Config: The parent configuration class with General confirguration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    Args:
+        Config: The parent configuration class with General confirguration settings
+    '''
+
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
